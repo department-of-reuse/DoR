29cf5eff0f521f8b80208d6dce691cc2524a0412
Ben Hermann <ben.hermann@googlemail.com>
2021-04-14 10:17:26 +0200

    Preparation for deployment to Github Pages

--- department-of-reuse/vue.config.js
+++ department-of-reuse/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/DoR'
+  : '/',
   configureWebpack: {
     devServer: {
       headers: { 'Access-Control-Allow-Origin': '*' }
