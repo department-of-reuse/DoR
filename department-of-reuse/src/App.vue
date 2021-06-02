6d51ebbbaf7713324b8014f24c4a0fcf8f2613a7
Ben Hermann <ben.hermann@googlemail.com>
2021-06-02 11:19:13 +0200

    Removed transition and added navigation bar (#68)

--- department-of-reuse/src/App.vue
+++ department-of-reuse/src/App.vue
@@ -1,47 +1,45 @@
 <template>
   <div class="w-full">
-      <transition name="fade" appear>
-        <img v-if="showSplash"  alt="Department of Reuse logo" src="./assets/dor-logo.svg" class="place-self-center w-1/3 mx-auto mt-10">
-        <div class="" v-else>
+    <img v-if="showSplash"  alt="Department of Reuse logo" src="./assets/dor-logo.svg" class="place-self-center w-1/3 mx-auto mt-10">
+    <div class="" v-else>
 
-          <div class="h-screen w-screen fixed bg-logo bg-no-repeat bg-cover bg-center">
-            <div class="h-full w-full bg-gray-50 bg-opacity-95 ">
-               <GraphView :reuseData="reuseData"/>
-            </div>
-          </div>
-
-          <div class="lg:flex mb-4 pt-5 px-5 relative">
-            <div class="flex-grow order-2">
-              <header class="bg-opacity-40 bg-gray-200 z-10">
-                <h1 class="text-4xl">Department of Reuse</h1>
-                <h2>- under development / data widely incomplete -</h2>
-              </header>
-            </div>
-            <div class="flex-none lg:w-72 order-1 w-full">
-              <h2 class="text-xl bg-opacity-80 bg-blue-200">Filter</h2>
-              <label>Reuse type</label>
-              <select class="" name="">
-                <option value="">All</option>
-                <option value="">Methodology</option>
-                <option value="">Dataset</option>
-                <option value="">Tool</option>
-                <option value="">Statistics</option>
-                <option value="">Protocol</option>
-                <option value="">Metric</option>
-              </select>
-            </div>
-            <div class="flex-none w-72 order-3 hidden lg:block" >
-                <TopFive class="z-10" :reuseData="reuseData" />
-            </div>
-          </div>
-          <footer class="mt-3 absolute -bottom-0 mx-auto w-full pb-3 bg-opacity-40 bg-gray-200">
-            <p class="text-xs">This is a community effort started by Maria Teresa Baldassarre, Neil Ernst, Ben Hermann, and Tim Menzies.</p>
-            <p class="text-xs">Please feel free to <a class="text-blue-600" href="https://github.com/bhermann/DoR">comment or contribute on GitHub</a>.</p>
-          </footer>
-
+      <div class="h-screen w-screen fixed bg-logo bg-no-repeat bg-cover bg-center">
+        <div class="h-full w-full bg-gray-50 bg-opacity-95 ">
+            <GraphView :reuseData="reuseData"/>
         </div>
+      </div>
 
-      </transition>
+      <Navigation />
+      <div class="lg:flex mb-4 pt-5 px-5 relative">
+        <div class="flex-grow order-2">
+          <header class="bg-opacity-40 bg-gray-200 z-10">
+            <h1 class="text-4xl">Department of Reuse</h1>
+            <h2>- under development / data widely incomplete -</h2>
+          </header>
+        </div>
+        <div class="flex-none lg:w-72 order-1 w-full">
+          <h2 class="text-xl bg-opacity-80 bg-blue-200">Filter</h2>
+          <label>Reuse type</label>
+          <select class="" name="">
+            <option value="">All</option>
+            <option value="">Methodology</option>
+            <option value="">Dataset</option>
+            <option value="">Tool</option>
+            <option value="">Statistics</option>
+            <option value="">Protocol</option>
+            <option value="">Metric</option>
+          </select>
+        </div>
+        <div class="flex-none w-72 order-3 hidden lg:block" >
+            <TopFive class="z-10" :reuseData="reuseData" />
+        </div>
+      </div>
+      <footer class="mt-3 absolute -bottom-0 mx-auto w-full pb-3 bg-opacity-40 bg-gray-200">
+        <p class="text-xs">This is a community effort started by Maria Teresa Baldassarre, Neil Ernst, Ben Hermann, and Tim Menzies.</p>
+        <p class="text-xs">Please feel free to <a class="text-blue-600" href="https://github.com/bhermann/DoR">comment or contribute on GitHub</a>.</p>
+      </footer>
+
+    </div>
   </div>
 </template>
 
@@ -50,12 +48,14 @@ import { ref, onMounted } from "vue";
 
 import GraphView from './components/GraphView.vue';
 import TopFive from './components/TopFive.vue';
+import Navigation from './components/Navigation.vue';
+
 import reuseJson from './assets/data/reuse.json';
 import { ReuseFromJson } from './backend/models/Reuse';
 
 export default {
   name: "App",
-  components: { GraphView, TopFive },
+  components: { GraphView, TopFive, Navigation },
   setup() {
     let showSplash = ref(true);
     const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
@@ -89,7 +89,7 @@ export default {
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.3s ease;
+  transition: opacity 0.3s;
 }
 
 .fade-enter-from,
---
+++ department-of-reuse/src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="nav" class="h-6 bg-opacity-80 bg-blue-500 text-white text-xs font-bold relative flex px-5 py-1">
+        <div class="flex-auto text-left flex gap-1.5">
+            <a class="hover:bg-blue-400 px-1" href="https://github.com/bhermann/DoR">Source code</a>
+            <!-- <a class="hover:bg-blue-400 px-1" href="https://github.com/bhermann/DoR">Meet the team</a> -->
+        </div>
+        <div class="flex-none">The Department of Reuse is a department within the ROSE initiative.</div>
+    </div>
+</template>
+<script lang="ts">
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style scoped>
+    #nav {
+        font-family: "Roboto Condensed", "Helvetica", "Arial", sans-serif;
+    }
+</style>
