c8cedb84d8a65fb54beaef40f1f6c3704f982eaa
Ben Hermann <ben.hermann@googlemail.com>
2021-07-19 09:50:35 +0200

    Reusing cache data

--- department-of-reuse/scripts/prefill-cache.ts
+++ department-of-reuse/scripts/prefill-cache.ts
@@ -1,18 +1,24 @@
 import reuseJson from '../src/assets/data/reuse.json';
+import currentCache from '../src/assets/data/works-cache.json';
+
 import { ReuseFromJson } from '../src/backend/models/Reuse';
-import { Configuration, WorksApi, Work, WorkMessage, WorkToJSON } from '../src/clients/crossref';
+import { Configuration, WorksApi, Work, WorkMessage, WorkToJSON, WorkFromJSON } from '../src/clients/crossref';
 import fetch from "node-fetch";
 import * as fs from 'fs';
 import pThrottle from 'p-throttle';
 
 console.log("Prefilling CrossRef resolution cache.")
 
+const cacheData = ((currentCache as Array<any>).map(WorkFromJSON));
 const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
 
 const crossRef = new WorksApi(new Configuration({
     fetchApi: fetch
 }));
 var cache: { doi: string, result: Work }[] = [];
+cacheData.forEach( currentWork => cache.push({doi: currentWork.dOI, result: currentWork}));
+
+console.log(`Reusing ${cacheData.length} previously cached items.`);
 
 const throttle = pThrottle({
     limit: 2,
@@ -31,9 +37,10 @@ const throttled = throttle(currentDoi => {
 const dois = Array.from(new Set(reuseData
                 .map(entry => entry.sourceDOI)
                 .concat(reuseData.map(entry => entry.reusedDOI))
-                .filter(doi => doi.trim() != "")));
+                .filter(doi => doi.trim() != "")))
+                .filter(doi => cache.findIndex(item => item.doi.toLowerCase() == doi.toLowerCase()) == -1)
 
-console.log(`Resolving ${dois.length} DOI(s).`);
+console.log(`Resolving ${dois.length} new DOI(s).`);
 
 Promise.all(dois.map(throttled)).then(entries => {
     entries.forEach(entry => {
@@ -52,4 +59,4 @@ Promise.all(dois.map(throttled)).then(entries => {
 
     fs.writeFileSync('./src/assets/data/works-cache.json', "[" + outputObject + "]");
 
-}).then(_ => console.log("CrossRef cache prefill complete."));
+}).then(_ => console.log(`CrossRef cache prefill complete. Wrote ${cache.length} item(s).`));
