96f3d41e1f39563f7060908b0f03629446b93c91
Ben Hermann <ben.hermann@googlemail.com>
2021-07-21 11:43:46 +0200

    Bugfix for arXiv client

--- department-of-reuse/src/clients/arxiv/models/Entry.ts
+++ department-of-reuse/src/clients/arxiv/models/Entry.ts
@@ -25,15 +25,27 @@ export function EntryFromXmlTyped(xml: any, ignoreDiscriminator: boolean) {
     if ((xml === undefined) || (xml === null)) {
         return xml;
     }
+    
+    // Bugfix: Category, Link, and Author may either be an array or single category. Normalizing to arrays here.
+    if (xml.author != null && !Array.isArray(xml.author))
+        xml.author = new Array(xml.author);
+
+    if (xml.link != null && !Array.isArray(xml.link))
+        xml.link = new Array(xml.link);
+
+    if (xml.category != null && !Array.isArray(xml.category))
+        xml.category = new Array(xml.category);
+
+    
     return {
         id: xml.id,
         updated: xml.updated,
         published: xml.published,
         title: xml.title,
         summary: xml.summary,
-        author: ((xml.author as Array<any>).map(AuthorFromXml)),
+        author: (xml.author == null) ? new Array() : ((xml.author as Array<any>).map(AuthorFromXml)),
         comment: CommentFromXml(xml['arxiv:comment']),
-        link: ((xml.link as Array<any>).map(LinkFromXml)),
+        link: (xml.link == null) ? new Array() : ((xml.link as Array<any>).map(LinkFromXml)),
         primary_category: CategoryFromXml(xml['arxiv:primary_category']),
         category: (xml.category == null) ? new Array() : ((xml.category as Array<any>).map(CategoryFromXml))
     };
