abb2874ef0500fd8e2f848ba3c30c6949c7406e7
Ben Hermann <ben.hermann@googlemail.com>
2021-08-13 15:45:56 +0200

    Added statistics (#94)
    - No. of source nodes
    - No. of target paper nodes
    - No. of target arXiv nodes
    - No. of target GitHub nodes
    - No. of target website nodes
    - Breakdown of reuse types

--- department-of-reuse/src/components/Stats.vue
+++ department-of-reuse/src/components/Stats.vue
@@ -1,31 +1,68 @@
 <template>
-    <div>
-      <h2 class="text-base bg-opacity-80 bg-blue-200 mt-3">Statistics</h2>
-      
-      <table class="m-1 table-auto text-xs bg-opacity-40 bg-white">
-        <tr>
-            <td class="w-full text-left">Papers inspected</td>
-            <td>{{ papersInspected }}</td>
-        </tr>
-        <tr>
-            <th></th>
-            <td></td>
-        </tr>
-      </table>
-    </div>
+  <div>
+    <h2 class="text-base bg-opacity-80 bg-blue-200 mt-3">Statistics</h2>
+
+    <table class="m-1 table-auto text-xs bg-opacity-40 bg-white">
+      <tr>
+        <td class="w-full text-left">Papers inspected</td>
+        <td class="text-right">{{ papersInspected }}</td>
+      </tr>
+      <tr>
+        <td class="w-full text-left">Reused papers (DOI)</td>
+        <td class="text-right">{{ targetDoiNodes }}</td>
+      </tr>
+      <tr>
+        <td class="w-full text-left">Reused papers (arXiv)</td>
+        <td class="text-right">{{ targetArxivNodes }}</td>
+      </tr>
+      <tr>
+        <td class="w-full text-left">Reused repositories (GitHub)</td>
+        <td class="text-right">{{ targetGitHubNodes }}</td>
+      </tr>
+      <tr>
+        <td class="w-full text-left">Reused websites</td>
+        <td class="text-right">{{ targetWebsiteNodes }}</td>
+      </tr>
+      <tr>
+        <td class="w-full text-center" colspan="2">Reuse types</td>
+      </tr>
+      <tr>
+        <td class="text-center" colspan="2">{{ edgeTypes }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 <script lang="ts">
 import { ref, PropType } from "vue";
-import Reuse from '../backend/models/Reuse';
+import Reuse from "../backend/models/Reuse";
+import { Histogram } from '../tools/Histogram';
+import CompoundSet from '../tools/CompoundSet';
 
-export default ({
-    props: {
-        reuseData: Array as PropType<Array<Reuse>>,
-    },
-    setup(props : any) {
-        const papersInspected = ref(new Set((props.reuseData as Array<Reuse>).map(r => r.sourceDOI)).size);
-        
-        return { papersInspected }
-    },
-})
+export default {
+  props: {
+    reuseData: Array as PropType<Array<Reuse>>,
+  },
+  setup(props: any) {
+    const data = props.reuseData as Array<Reuse>;
+
+    const papersInspected = ref(new Set(data.map((r) => r.sourceDOI)).size);
+
+    const targetDoiNodes = ref(new Set(data.filter(r => r.reusedDOI.trim().length > 0).map(r => r.reusedDOI)).size);
+    const targetGitHubNodes = ref(new Set(data.filter(r => r.alternativeID.startsWith("https://github.com")).map(r => r.alternativeID)).size);
+    const targetArxivNodes = ref(new Set(data.filter(r => r.alternativeID.startsWith("arxiv:")).map(r => r.alternativeID)).size);
+    
+    const websiteFilter = (id: string): boolean => !id.startsWith("https://github.com/")&&(id.startsWith("http://")||id.startsWith("https://"));
+    const targetWebsiteNodes = ref(new Set(data.filter(r => websiteFilter(r.alternativeID)).map(r => r.alternativeID)).size);
+
+    const edges = Array.from(new CompoundSet(data.map((item: Reuse) => {
+        return { source: item.sourceDOI, target: item.reusedDOI.trim().length > 0 ? item.reusedDOI : item.alternativeID, type: item.type } ;
+    })));
+    
+    const edgeTypeHistogram = new Histogram(e => `${e.type}`, edges).histogram();
+
+    const edgeTypes = edgeTypeHistogram.map(e => `${e.entry.type} (${e.frequency})`).join(", ");
+
+    return { papersInspected, targetDoiNodes, targetGitHubNodes, targetArxivNodes, targetWebsiteNodes, edgeTypes };
+  },
+};
 </script>
