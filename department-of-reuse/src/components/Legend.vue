55646cc4067cc172be59f80ce5ac51e4d9c844fe
Ben Hermann <ben.hermann@googlemail.com>
2021-08-13 16:58:56 +0200

    Added Legend (#93 )

---
+++ department-of-reuse/src/components/Legend.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="legend">
+    <h2 class="text-base bg-opacity-80 bg-blue-200 mt-3">Legend</h2>
+
+    <table class="m-1 table-auto text-xs bg-opacity-40 bg-white w-full text-left">
+      <tr>
+        <td><div style="background-color: #77aaff" class="w-4 h-4 rounded-full mx-auto"></div></td>
+        <td>Published Paper</td>
+      </tr>
+      <tr>
+        <td><div style="background-color: #b31b1b" class="w-4 h-4 rounded-full mx-auto"></div></td>
+        <td>arXiv preprint</td>
+      </tr>      
+      <tr>
+        <td><div style="background-color: #238636" class="w-4 h-4 rounded-full mx-auto"></div></td>
+        <td>GitHub repository</td>
+      </tr>      
+      <tr>
+        <td><div style="background-color: #999" class="w-4 h-4 rounded-full mx-auto"></div></td>
+        <td>Website</td>
+      </tr>      
+    </table>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
--- department-of-reuse/src/pages/Home.vue
+++ department-of-reuse/src/pages/Home.vue
@@ -28,6 +28,7 @@
         <div class="flex-none w-72 order-3 hidden lg:block z-10 overflow-y-auto h-screen" >
             <TopFive class="z-10" :reuseData="reuseData" />
             <Stats :reuseData="reuseData" />
+            <Legend />
         </div>
       </div>
     </div>
@@ -36,15 +37,15 @@
 <script lang="ts">
 import GraphView from '../components/GraphView.vue';
 import TopFive from '../components/TopFive.vue';
-
 import Stats from '../components/Stats.vue';
+import Legend from '../components/Legend.vue';
 
 import reuseJson from '../assets/data/reuse.json';
 import { ReuseFromJson } from '../backend/models/Reuse';
 
 export default {
   name: "Home",
-  components: { Stats, TopFive, GraphView },
+  components: { Stats, TopFive, GraphView, Legend },
   setup() {
     const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
 
