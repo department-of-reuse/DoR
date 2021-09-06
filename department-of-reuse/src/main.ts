201411297e64dfbf4ed4cada1ad59e8daf7fc16f
Ben Hermann <ben.hermann@googlemail.com>
2021-09-06 16:38:00 +0200

    Removed axios from dependencies as it was unused

--- department-of-reuse/src/main.ts
+++ department-of-reuse/src/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import router from './tools/router'
 
 const app = createApp(App)
-app.use(VueAxios, axios)
 app.use(router)
 app.mount('#app')
