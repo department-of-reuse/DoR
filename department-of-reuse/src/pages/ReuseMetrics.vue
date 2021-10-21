aa55a4f2cd0fe214b09e9fa00ff07a38cb2e65d8
Johannes Düsing <j.duesing13@googlemail.com>
2021-10-21 16:49:38 +0200

    Add first version of table displaying R metrics

--- department-of-reuse/src/pages/ReuseMetrics.vue
+++ department-of-reuse/src/pages/ReuseMetrics.vue
@@ -1,3 +1,7 @@
+<style lang="scss">
+  @import "~ag-grid-community/dist/styles/ag-grid.css";
+  @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
+</style>
 <template>
   <div>
     <h1 class="text-xl bg-opacity-80 bg-blue-200">Collected Reuse Statistics</h1>
@@ -7,48 +11,73 @@
     <div v-else class="">
         <p>Lorem ipsum</p>
         <p>Lorem ipsum2</p>
+        <ag-grid-vue
+            style="width: 1000px; height: 500px"
+            class="ag-theme-alpine"
+            :columnDefs="columns"
+            :rowData="researchers.reused"
+          >
+        </ag-grid-vue>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, onBeforeMount, ref } from "vue";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-import reuseJson from "../assets/data/reuse.json";
-import Reuse, { ReuseFromJson } from "../backend/models/Reuse";
+import { defineComponent, onBeforeMount, ref } from "vue";
+import { AgGridVue } from "ag-grid-vue3";
+
+import reuseJson from '../assets/data/reuse.json';
+import { ReuseFromJson } from '../backend/models/Reuse';
+
+import RIndex from '../backend/RIndex';
+
+type Row = {
+  name: string,
+  rvalue: number
+}
+
+interface HistogramContainer {
+  reused : Array<Row>,
+  reusing : Array<any>
+}
+
+
+
 
 export default defineComponent({
   name: "ReuseMetrics",
-  components: {  },
+  components: { AgGridVue },
   setup() {
     const isLoading = ref(false);
-    const rows = ref(new Array<Reuse>());
+    const researchers = ref({} as HistogramContainer);
+    const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
+    const indexer = new RIndex(reuseData);
 
     onBeforeMount(async () => {
         isLoading.value = true;
         
-        const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
-
-        rows.value = reuseData;
+        researchers.value = { 
+          reused: (await indexer.computeAuthorsReused()).map(p => {return {name: p.entry.given! + " " + p.entry.family!, rvalue: p.frequency}}),
+          reusing: (await indexer.computeAuthorsReusing())
+          };
 
         isLoading.value = false;
     })
     
     const columns = [
         {
-          prop: "sourceDOI",
-          name: "Source Paper",
+          field: "name",
+          headerName: "Autor",
         },
         {
-          prop: "type",
-          name: "Type",
-        },
-        {
-          prop: "reusedDOI",
-          name: "Target Paper",
+          field: "rvalue",
+          headerName: "Reused(R)",
         }
     ];
 
-    return { isLoading, columns, rows };
+    return { isLoading, columns, researchers };
   },
 });
 </script>
