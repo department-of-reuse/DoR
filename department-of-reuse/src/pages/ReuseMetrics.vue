67961de49d53cab3051716c6764cc2973c4cded9
Johannes Düsing <j.duesing13@googlemail.com>
2021-10-22 12:37:35 +0200

    Working version for Authors and Publications

--- department-of-reuse/src/pages/ReuseMetrics.vue
+++ department-of-reuse/src/pages/ReuseMetrics.vue
@@ -1,23 +1,43 @@
 <style lang="scss">
   @import "~ag-grid-community/dist/styles/ag-grid.css";
   @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+  .cell-wrap-text { white-space: normal !important; }
 </style>
 <template>
   <div>
-    <h1 class="text-xl bg-opacity-80 bg-blue-200">Collected Reuse Statistics</h1>
+    <h1 class="text-xl bg-opacity-80 bg-blue-200">Reuse Statistics for Authors and Publications</h1>
     <div v-if="isLoading">
       <p>Loading data ...</p>
     </div>
-    <div v-else class="">
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum2</p>
-        <ag-grid-vue
-            style="width: 1000px; height: 500px"
-            class="ag-theme-alpine"
-            :columnDefs="columns"
-            :rowData="rowData"
-          >
-        </ag-grid-vue>
+    <div v-else class="h-screen">
+        <div style="width: 40%; float:left; height: 85vh; margin-top:1vh; margin-left: 2%">
+          <h1 class="text-3xl">Reuse Statistics by Author</h1>
+          <ag-grid-vue
+              style="width: 100%; height: 97%; margin-top: 3%"
+              class="ag-theme-alpine"
+              :columnDefs="columns.authors"
+              :rowData="rowData.authors"
+              :animateRows="true"
+              :defaultColDef="defaultColDef"
+              :enableCellTextSelection="true"
+            >
+          </ag-grid-vue>
+        </div>
+        <div style="width: 54%; float:left; height: 85vh; margin-top:1vh; margin-left: 2%;">
+          <h1 class="text-3xl">Reuse Statistics by Publication</h1>
+          <ag-grid-vue
+              style="width: 100%; height: 97%; margin-top: 2.2%"
+              class="ag-theme-alpine"
+              :columnDefs="columns.papers"
+              :rowData="rowData.papers"
+              :animateRows="true"
+              :defaultColDef="defaultColDef"
+              :enableCellTextSelection="true"
+            >
+          </ag-grid-vue>
+        </div>
+        <div style="clear: both;"></div>
     </div>
   </div>
 </template>
@@ -29,25 +49,37 @@ import { defineComponent, onBeforeMount, ref } from "vue";
 import { AgGridVue } from "ag-grid-vue3";
 
 import { HistogramEntry } from '../tools/Histogram';
-import { Author } from '../clients/crossref';
+import { Author, Work } from '../clients/crossref';
 
 import reuseJson from '../assets/data/reuse.json';
 import { ReuseFromJson } from '../backend/models/Reuse';
 
 import RIndex from '../backend/RIndex';
 
-type Row = {
-  name: string,
+type AuthorRow = {
+  givenname: string,
+  familyname: string,
   reusedvalue: number,
   reusingvalue: number
 }
 
-interface HistogramContainer {
-  reused : Array<HistogramEntry<Author>>,
-  reusing : Array<HistogramEntry<Author>>
+type PublicationRow = {
+  name: String,
+  reusedvalue: number,
+  reusingvalue: number
 }
 
-function getFrequencyOrElseZero(entry: HistogramEntry<Author> | undefined): number {
+interface HistogramContainer<T> {
+  reused : Array<HistogramEntry<T>>,
+  reusing : Array<HistogramEntry<T>>
+}
+
+interface RowContainer {
+  authors: Array<AuthorRow>,
+  papers: Array<PublicationRow>
+}
+
+function getFrequencyOrElseZero<T>(entry: HistogramEntry<T> | undefined): number {
   if(entry === undefined){
     return 0
   } else {
@@ -61,50 +93,120 @@ export default defineComponent({
   components: { AgGridVue },
   setup() {
     const isLoading = ref(false);
-    const researchers = ref({} as HistogramContainer);
-    const rowData = ref({} as Array<Row>)
+    const rowData = ref({authors:[], papers:[]} as RowContainer)
     const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
     const indexer = new RIndex(reuseData);
 
     onBeforeMount(async () => {
         isLoading.value = true;
         
-        researchers.value = { 
+        let researchers: HistogramContainer<Author> = { 
           reused: (await indexer.computeAuthorsReused()),
           reusing: (await indexer.computeAuthorsReusing())
           };
+    
+        let papers: HistogramContainer<Work> = {
+          reused: (await indexer.computeWorksReused()),
+          reusing: (await indexer.computeWorksReusing())
+          };
+        
 
-        let allAuthors: Array<Author> = Array.from(new Set(researchers.value.reused.map(p => p.entry).concat(researchers.value.reusing.map(p => p.entry))))
+        let allAuthors: Array<Author> = Array.from(new Set(researchers.reused.map(p => p.entry).concat(researchers.reusing.map(p => p.entry))))
+        let allWorks: Array<Work> = Array.from(new Set(papers.reused.map(p => p.entry).concat(papers.reusing.map(p => p.entry))))
 
-        rowData.value = allAuthors
+
+        rowData.value.authors = allAuthors
           .map(author => {
-            let nameVal = author.given! + " " + author.family!
+            let givennameVal = author.given == undefined ? "" : author.given
+            let familynameVal = author.family == undefined ? "" : author.family
 
-            let reusedVal = getFrequencyOrElseZero(researchers.value.reused.find(e => { return e.entry.given == author.given && e.entry.family == author.family }))
-            let reusingVal = getFrequencyOrElseZero(researchers.value.reusing.find(e => { return e.entry.given == author.given && e.entry.family == author.family }))
+            let reusedVal = getFrequencyOrElseZero(researchers.reused.find(e => { return e.entry == author }))
+            let reusingVal = getFrequencyOrElseZero(researchers.reusing.find(e => { return e.entry == author }))
 
-            return {name: nameVal, reusedvalue: reusedVal, reusingvalue: reusingVal}
+            return {givenname: givennameVal, familyname: familynameVal, reusedvalue: reusedVal, reusingvalue: reusingVal}
+          })
+
+        rowData.value.papers = allWorks
+          .map(work => {
+            let title = work.title[0]
+
+            let reusedVal = getFrequencyOrElseZero(papers.reused.find(e => { return e.entry.dOI == work.dOI }))
+            let reusingVal = getFrequencyOrElseZero(papers.reusing.find(e => { return e.entry.dOI == work.dOI }))
+
+            return {name: title, reusedvalue: reusedVal, reusingvalue: reusingVal}
           })
 
         isLoading.value = false;
     })
+
+    let defaultColDef = {
+        sortable: true,
+        sortingOrder: ["asc", "desc"],
+        filterParams: {
+          buttons: ["reset"],
+          debounceMs: 300
+        }
+      }
     
-    const columns = [
+    const columns = {
+      authors:[
+        {
+          field: "givenname",
+          headerName: "Given Name",
+          filter: "agTextColumnFilter",
+          resizable: true,
+          flex:2
+        },
+        {
+          field: "familyname",
+          headerName: "Family Name",
+          filter: "agTextColumnFilter",
+          resizable: true,
+          flex:2
+        },
+        {
+          field: "reusedvalue",
+          headerName: "R+",
+          headerTooltip: "Times publications of this author have been reused.",
+          filter: "agNumberColumnFilter",
+          flex:1
+        },
+        {
+          field: "reusingvalue",
+          headerName: "R",
+          headerTooltip: "Times this author has reused other publications.",
+          filter: "agNumberColumnFilter",
+          flex:1
+        }
+      ],
+      papers:[
         {
           field: "name",
-          headerName: "Autor",
+          headerName: "Publication",
+          filter: "agTextColumnFilter",
+          resizable: true,
+          flex:3
+          //cellClass: "cell-wrap-text",
+          //autoHeight: true
         },
         {
           field: "reusedvalue",
           headerName: "Reused(R+)",
+          filter: "agNumberColumnFilter",
+          flex:1
         },
         {
           field: "reusingvalue",
           headerName: "Reusing(R)",
+          filter: "agNumberColumnFilter",
+          flex:1
         }
-    ];
+      ]
+    }
+    
+    ;
 
-    return { isLoading, columns, rowData };
+    return { isLoading, columns, rowData, defaultColDef };
   },
 });
 </script>
