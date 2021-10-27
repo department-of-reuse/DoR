61a78da34d80e4a3bac4a3600ef55a05fefe217d
Ben Hermann <ben.hermann@googlemail.com>
2021-10-27 21:15:34 +0200

    Removed inactive import

--- department-of-reuse/src/tools/router/index.ts
+++ department-of-reuse/src/tools/router/index.ts
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from '../../pages/Home.vue';
 import Contributors from '@/pages/Contributors.vue';
 import Paper from '@/pages/Paper.vue';
-import ReuseData from '@/pages/ReuseData.vue';
 import ReuseMetrics from '@/pages/ReuseMetrics.vue'
 
 const routerHistory = createWebHistory(process.env.BASE_URL);
