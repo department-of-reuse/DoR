0a30ca23433bb23f935399bcd6ce7bc82aabfff9
Ben Hermann <ben.hermann@googlemail.com>
2022-03-06 15:01:12 +0100

    Usability improvements and bugfixes

--- department-of-reuse/src/components/EditDialog.vue
+++ department-of-reuse/src/components/EditDialog.vue
@@ -2,13 +2,14 @@
   <div class="fixed w-screen h-screen z-20 bg-black bg-opacity-60">
     <div
       class="
-        w-1/3
+        w-1/2
         bg-gray-100
         mx-auto
         inset-x-0
-        mt-20
+        mt-5
         border-4 border-gray-100
         pb-5
+        h-max
       "
       v-if="!isLoading"
     >
@@ -27,20 +28,57 @@
               text-white
               p-1
             "
+            title="Removes the stored GitHub access token from your browsers local storage."
           >
             Forget token
           </button>
         </div>
 
-        <h3 class="bg-opacity-80 bg-blue-100 mb-5 text-base text-center mt-5">
+        <h3 class="bg-opacity-80 bg-blue-100 text-base text-center mt-3">
           Report Reuse
         </h3>
+
+        <div
+          class="
+            w-full
+            h-full
+            bg-green-100
+            text-xs text-green-800
+            mt-2
+            py-1
+            px-2
+          "
+          v-if="submitSuccess"
+        >
+          ✅ Report submitted successfully.
+          <a :href="lastIssueUrl" target="_blank" class="text-blue-500"
+            >View issue.</a
+          >
+        </div>
+
+        <p class="text-xs mt-3">
+          Based on a publication identified by its
+          <a href="https://www.doi.org/" target="_blank" class="text-blue-500"
+            >DOI</a
+          >
+          you can report any reuse of research artifacts or results presented in
+          the paper. When you enter the DOI below details for this publication
+          will be retrieved.
+        </p>
+
         <input
           type="text"
           placeholder="DOI (e.g. 10.1145/3368089.3409767)"
           class="w-full mt-3 mb-3 p-2"
           v-model="reuse.sourceDoi"
         />
+
+        <p v-if="loadingPublication" class="bg-blue-300 text-blue-900 p-3 mb-3">
+          Loading publication details...
+        </p>
+        <p v-if="invalidDOI" class="bg-yellow-300 text-yellow-900 p-3 mb-3">
+          Invalid DOI used.
+        </p>
 
         <div v-if="sourceWork.dOI">
           <p>
@@ -69,9 +107,10 @@
               rounded-lg
               shadow-lg
               text-white
+              mt-1
             "
           >
-            Add reuse
+            Add row
           </button>
 
           <ag-grid-vue
@@ -83,7 +122,7 @@
             :enableCellTextSelection="true"
             @grid-ready="onGridReady"
             @cell-value-changed="onCellValueChanged"
-            :tooltipShowDelay = "500"
+            :tooltipShowDelay="500"
           >
           </ag-grid-vue>
 
@@ -147,7 +186,10 @@
 
         <p class="mt-2">
           You can create such a personal access token at
-          <a href="https://github.com/settings/tokens" class="text-blue-500"
+          <a
+            href="https://github.com/settings/tokens"
+            class="text-blue-500"
+            target="_blank"
             >https://github.com/settings/tokens</a
           >. Please set the following permissions:
           <span class="font-mono">public_repo</span>
@@ -161,7 +203,10 @@
 
         <p class="mt-2">
           We will use your token to create issues in our own repository
-          <a class="text-blue-500" :href="`https://github.com/${owner}/${repo}`"
+          <a
+            class="text-blue-500"
+            :href="`https://github.com/${owner}/${repo}`"
+            target="_blank"
             >{{ owner }}/{{ repo }}</a
           >
           directly from your browser.
@@ -175,6 +220,10 @@
           placeholder="GitHub token goes here"
           v-model="token"
         />
+
+        <p v-if="invalidToken" class="mb-3 bg-yellow-300 text-yellow-900 p-3">
+          The token is not valid (currently).
+        </p>
 
         <div class="grid grid-cols-2 gap-4">
           <button
@@ -222,6 +271,7 @@ import { AgGridVue } from "ag-grid-vue3";
 import { $enum } from "ts-enum-util";
 import { ReuseType } from "../backend/models/Reuse";
 import {
+  CellClassParams,
   CellValueChangedEvent,
   GridApi,
   GridReadyEvent,
@@ -234,10 +284,10 @@ interface Reuse {
 }
 
 interface SingleReuse {
-  reused: string,
-  type: string,
-  sourceRef: string,
-  comment: string,
+  reused: string;
+  type: string;
+  sourceRef: string;
+  comment: string;
 }
 
 export default defineComponent({
@@ -249,7 +299,12 @@ export default defineComponent({
 
     const isLoading = ref(false);
     const tokenValid = ref(false);
+    const invalidToken = ref(false);
+    const loadingPublication = ref(false);
+    const invalidDOI = ref(false);
+    const submitSuccess = ref(false);
     const token = ref("");
+    const lastIssueUrl = ref("");
     const githubUser = ref({}) as Ref<{ login?: string }>;
     const reuse = ref({ reuse: [] }) as Ref<Reuse>;
 
@@ -277,7 +332,13 @@ export default defineComponent({
         editable: true,
         cellEditorPopup: false,
         headerTooltip: `Provide the identifier of the reused object. 
-This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.06821), a GitHub repository (e.g. ${owner}/${repo}), or a URL.`
+This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.06821), a GitHub repository (e.g. ${owner}/${repo}), or a URL.`,
+        cellClassRules: {
+          'bg-paper text-white': (params : CellClassParams) => { return classifyReuse(params.value) == ReuseClass.DOI; },
+          'bg-arxiv  text-white': (params : CellClassParams) => { return classifyReuse(params.value) == ReuseClass.ARXIV; },
+          'bg-github text-white': (params : CellClassParams) => { return classifyReuse(params.value) == ReuseClass.GITHUB; },
+          'bg-website  text-white': (params : CellClassParams) => { return classifyReuse(params.value) == ReuseClass.WEBSITE; }
+        }
       },
       {
         headerName: "Type",
@@ -290,7 +351,7 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
           cellHeight: 50,
           values: reuseTypes.map((r) => r[1]),
         },
-        headerTooltip: "The type of reuse"
+        headerTooltip: "The type of reuse",
       },
       {
         headerName: "Ref",
@@ -298,7 +359,7 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
         width: 50,
         editable: true,
         cellEditorPopup: false,
-        headerTooltip: "Reference in the paper reusing the object"
+        headerTooltip: "Reference in the paper reusing the object (optional)",
       },
       {
         headerName: "Comment",
@@ -306,7 +367,7 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
         flex: 1,
         editable: true,
         cellEditorPopup: false,
-        headerTooltip: "Additional information"
+        headerTooltip: "Additional information (optional)",
       },
     ];
 
@@ -335,6 +396,7 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
     };
 
     const checkToken = async () => {
+      invalidToken.value = false;
       const gh = new Octokit({
         auth: token.value,
       });
@@ -347,13 +409,14 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
         })
         .catch((e) => {
           console.log(e);
+          invalidToken.value = true;
           return false;
         });
       return valid;
     };
 
     const loadGitHubContent = async () => {
-      const gh = new Octokit({
+      /*const gh = new Octokit({
         auth: token.value,
       });
 
@@ -366,61 +429,84 @@ This can be a DOI (e.g. 10.1145/3368089.3409767), an arXiv id (e.g. arXiv:2108.0
             labels: label,
           })
         ).data
-      );
+      );*/
     };
 
     /* eslint-disable no-unused-vars */
     enum ReuseClass {
-      INVALID, 
-      WEBSITE, 
+      INVALID,
+      WEBSITE,
       DOI,
       GITHUB,
-      ARXIV 
+      ARXIV,
     }
     /* eslint-enable no-unused-vars */
-    
-    const sanitizeReuse = (reuse : string) => {
-      return reuse.replace("https://doi.org/", "")
-              .replace("https://dl.acm.org/doi/abs/", "")
-              .replace("https://dl.acm.org/doi/pdf/", "")
-              .replace("https://dl.acm.org/doi/", "")
-              .replace("http://dx.doi.org/", "")
-              .replace("https://dx.doi.org/", "")
-              .trim();
-    }
 
-    const doiRegExp : RegExp = new RegExp('(?:^' + '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)' + '$)');
-    const githubRegExp = new RegExp('([w.@:/-~]+)')
-    const classifyReuse = (reuse : string) => {
+    const sanitizeReuse = (reuse: string) => {
+      return reuse
+        .replace("https://doi.org/", "")
+        .replace("https://dl.acm.org/doi/abs/", "")
+        .replace("https://dl.acm.org/doi/pdf/", "")
+        .replace("https://dl.acm.org/doi/", "")
+        .replace("http://dx.doi.org/", "")
+        .replace("https://dx.doi.org/", "")
+        .trim();
+    };
+
+    const doiRegExp: RegExp = new RegExp(
+      "(?:^" + '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)' + "$)"
+    );
+    const githubRegExp = new RegExp("([w.@/-~]+)");
+    const classifyReuse = (reuse: string) => {
       const reuseValue = sanitizeReuse(reuse);
-      if (reuseValue.match(doiRegExp)) return ReuseClass.DOI   
-      if (reuseValue.startsWith("arxiv:") || reuseValue.startsWith("arXiv:")) return ReuseClass.ARXIV
-      if (reuseValue.match(githubRegExp)) return ReuseClass.GITHUB
-      if (reuseValue.startsWith("http://") || reuseValue.startsWith("https://")) return ReuseClass.WEBSITE
-      return ReuseClass.INVALID
-    }
+      if (reuseValue.match(doiRegExp)) return ReuseClass.DOI;
+      if (reuseValue.startsWith("arxiv:") || reuseValue.startsWith("arXiv:"))
+        return ReuseClass.ARXIV;
+      if (!reuseValue.startsWith("http://") && !reuseValue.startsWith("https://") && reuseValue.match(githubRegExp)) return ReuseClass.GITHUB;
+      if (reuseValue.startsWith("http://") || reuseValue.startsWith("https://"))
+        return ReuseClass.WEBSITE;
+      return ReuseClass.INVALID;
+    };
 
-    const transformReuse = (reuse : string) => {
-      const classification = classifyReuse(reuse)
-      if (classification == ReuseClass.DOI) return `[${reuse}](https://doi.org/${reuse})`
-      if (classification == ReuseClass.GITHUB) return `[${reuse}](https://github.com/${reuse})`
-      if (classification == ReuseClass.ARXIV) return `[${reuse}](https://arxiv.org/abs/${reuse.replace("arxiv:", "").replace("arXiv:", "")})`
-      if (classification == ReuseClass.WEBSITE)  return `[${reuse}](${reuse})`
-      return reuse 
-    }
+    const transformReuse = (reuse: string) => {
+      const classification = classifyReuse(reuse);
+      if (classification == ReuseClass.DOI)
+        return `[${reuse}](https://doi.org/${reuse})`;
+      if (classification == ReuseClass.GITHUB)
+        return `[${reuse}](https://github.com/${reuse})`;
+      if (classification == ReuseClass.ARXIV)
+        return `[${reuse}](https://arxiv.org/abs/${reuse
+          .replace("arxiv:", "")
+          .replace("arXiv:", "")})`;
+      if (classification == ReuseClass.WEBSITE) return `[${reuse}](${reuse})`;
+      return reuse;
+    };
 
-    const startRequest = () => {
+    const startRequest = async () => {
       const gh = new Octokit({
         auth: token.value,
       });
 
-      const tableRows = reuse.value.reuse.map(r => `| ${transformReuse(r.reused)} | ${r.type} | ${r.sourceRef} | ${r.comment} |`).join("\n")
-      const csvRows = reuse.value.reuse.map(r => {
-        const classification = classifyReuse(r.reused)
-        const sanized = sanitizeReuse(r.reused)
-        return `${sourceWork.value.dOI},${classification == ReuseClass.DOI ? sanized : ""},${r.type},${r.comment},${r.sourceRef},${classification != ReuseClass.DOI ? sanized : ""},,${githubUser.value.login}`
-      }).join("\n")
-      const body = `Reporting reuse in the following paper: 
+      const tableRows = reuse.value.reuse
+        .map(
+          (r) =>
+            `| ${transformReuse(r.reused)} | ${r.type} | ${r.sourceRef} | ${
+              r.comment
+            } |`
+        )
+        .join("\n");
+      const csvRows = reuse.value.reuse
+        .map((r) => {
+          const classification = classifyReuse(r.reused);
+          const sanized = sanitizeReuse(r.reused);
+          return `${sourceWork.value.dOI},${
+            classification == ReuseClass.DOI ? sanized : ""
+          },${r.type},${r.comment},${r.sourceRef},${
+            classification != ReuseClass.DOI ? sanized : ""
+          },,${githubUser.value.login}`;
+        })
+        .join("\n");
+      const body = `Reporting reuse in the following publication: 
 ${sourceWork.value.title.join(" ")}
 by ${sourceWork.value.author.map((a) => `${a.given} ${a.family}`).join(", ")}
 DOI: [${sourceWork.value.dOI}](https://doi.org/${sourceWork.value.dOI})
@@ -435,7 +521,7 @@ ${csvRows}
 \`\`\`
 This issue has been created using the [Department of Reuse website](https://www.reuse-dept.org).`;
 
-      gh.rest.issues.create({
+      const result = await gh.rest.issues.create({
         owner: owner,
         repo: repo,
         title: `[Reuse in] ${sourceWork.value.title.join(" ")}`,
@@ -443,6 +529,16 @@ This issue has been created using the [Department of Reuse website](https://www.
         labels: [label],
       });
 
+      lastIssueUrl.value = result.data.html_url;
+
+      submitSuccess.value = true;
+
+      const req = new Promise(() =>
+        setTimeout(async () => {
+          submitSuccess.value = false;
+        }, 10000)
+      );
+      req.then(() => console.log("Info box reset."));
       reuse.value = { reuse: [] };
       sourceWork.value = {} as Work;
     };
@@ -455,7 +551,7 @@ This issue has been created using the [Department of Reuse website](https://www.
 
     const addRow = () => {
       gridApi.applyTransaction({
-        add: [{ reused: "", type: "", comment: "" }],
+        add: [{ reused: "", type: "", sourceRef: "", comment: "" }],
       });
     };
 
@@ -486,12 +582,19 @@ This issue has been created using the [Department of Reuse website](https://www.
       (newValue) => {
         if (newValue && newValue != "" && !sourceDoiRequested) {
           sourceDoiRequested = true;
+          loadingPublication.value = true;
+          invalidDOI.value = false;
+          sourceWork.value = {} as Work;
           const req = new Promise(() =>
             setTimeout(async () => {
               const response = await worksApi
                 .worksDoiGetInteral({ doi: newValue }, true)
-                .catch((e) => console.warn(e));
+                .catch((e) => {
+                  invalidDOI.value = true;
+                  console.warn(e);
+                });
               sourceDoiRequested = false;
+              loadingPublication.value = false;
               if (response) sourceWork.value = response.message;
             }, 1200)
           );
@@ -502,9 +605,14 @@ This issue has been created using the [Department of Reuse website](https://www.
 
     return {
       tokenValid,
+      invalidToken,
+      invalidDOI,
+      loadingPublication,
       token,
       isLoading,
+      submitSuccess,
       githubUser,
+      lastIssueUrl,
       reuse,
       sourceWork,
       columnDefs,
--- department-of-reuse/tailwind.config.js
+++ department-of-reuse/tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
     theme: {
         extend: {
+            colors: {
+                'paper': '#77aaff',
+                'arxiv': '#b31b1b',
+                'github': '#238636',
+                'website': '#999'
+            },
             backgroundImage: theme => ({
                 'logo': "url('../assets/dor-logo.svg')",
-            })
+            }),
         }
     }
 }
